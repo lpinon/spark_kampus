ae5739e21074999ea270b025fe2c673f128ebd6d
Luis Piñon Ferrer <luis.pinanferrer@nike.com>
2021-04-28 00:35:36 +0200

    Added postgresql_connector.py connector

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import main.config.constants as Constants
 def main():
     # Configure Spark Session
     config = {
-        "spark.jars.packages": "io.delta:delta-core_2.12:0.8.0,org.postgresql:postgresql:9.4.1211",
+        "spark.jars.packages": "io.delta:delta-core_2.12:0.8.0,"
+                               "org.postgresql:postgresql:9.4.1211,"
+                               "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1",
         "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
         "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
         Constants.CURRENT_DATA_DELTA_TABLE_NAME: Constants.CURRENT_DATA,
