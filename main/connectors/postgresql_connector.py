450502a9c07a14b3563b6e0301d74368e990d5e9
Luis Piñon Ferrer <luis.pinanferrer@nike.com>
2021-04-28 00:14:45 +0200

    Added driver postgres direct client

--- main/connectors/postgresql_connector.py
+++ main/connectors/postgresql_connector.py
@@ -16,7 +16,7 @@ class PostgreSQLConnector:
         self.jdbc = "jdbc:postgresql://{0}/{1}?user={2}&password={3}".format(self.host, self.database, self.username,
                                                                              self.password)
 
-    def get_table(self, table: str)->DataFrame:
+    def get_table(self, table: str) -> DataFrame:
         return self.config.spark_session.read.format('jdbc').options(
             url=self.jdbc,
             dbtable=table,
@@ -36,19 +36,20 @@ class PostgreSQLConnector:
     def store_only_new_records(self, df: DataFrame, table: str, id_col: str):
         new_data = df.select(col(id_col)).withColumn("needs_insert", lit(True))
         existing_data = self.get_table(table).select(col(id_col)).withColumn("is_new", lit(False))
-        data_to_insert = existing_data\
-          .join(broadcast(new_data), [id_col], "right_outer")\
-          .withColumn("to_insert", coalesce(col("is_new"), col("needs_insert")))\
-          .filter(col("to_insert").equalTo(lit(True)))\
-          .select(col(id_col))\
-          .distinct()
+        data_to_insert = existing_data \
+            .join(broadcast(new_data), [id_col], "right_outer") \
+            .withColumn("to_insert", coalesce(col("is_new"), col("needs_insert"))) \
+            .filter(col("to_insert").equalTo(lit(True))) \
+            .select(col(id_col)) \
+            .distinct()
         new_dataframe_to_store = df.join(broadcast(data_to_insert), [id_col], "right_outer")
         self.store(new_dataframe_to_store, table)
 
     def delete_in(self, table: str, id_col: str, ids_to_delete: list):
         ids_query = ", ".join(["'{}'".format(el) for el in ids_to_delete])
         if len(ids_query) > 0:
-            connection = psycopg2.connect(dbname=self.database, user=self.username, password=self.password, host=self.host)
+            connection = psycopg2.connect(dbname=self.database, user=self.username, password=self.password,
+                                          host=self.host)
             with connection:
                 with connection.cursor() as curs:
                     curs.execute("DELETE FROM {0} WHERE {1} IN {2}".format(table, id_col, ids_query))
