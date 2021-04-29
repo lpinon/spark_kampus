5f8931d01115802794c600f3476035562ba82a70
Luis Piñon Ferrer <luis.pinanferrer@nike.com>
2021-04-29 12:03:33 +0200

    Added Java Heap control

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
         "spark.jars.packages": "io.delta:delta-core_2.12:0.8.0,"
                                "org.postgresql:postgresql:9.4.1211,"
                                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1",
+        "spark.driver.memory": "8g",
         "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
         "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
         Constants.CURRENT_DATA_DELTA_TABLE_NAME: Constants.CURRENT_DATA,
