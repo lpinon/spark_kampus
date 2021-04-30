07ee567e675c8d2e30937d30f47501e31f42ad41
Luis Piñon Ferrer <luis.pinanferrer@nike.com>
2021-04-30 21:12:28 +0200

    Basic visit ingestion

--- main/processors/normalizer.py
+++ main/processors/normalizer.py
@@ -1,6 +1,18 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp, col, concat, lit
+import main.config.constants as Constants
 
 
-def normalize(df_new_data: DataFrame) -> DataFrame:
-    pass
+def add_visit_id(df_visit: DataFrame) -> DataFrame:
+    return df_visit.withColumn(Constants.VISITS_ID, concat(
+        col(Constants.VISITS_USER_ID), lit("_"),
+        col(Constants.VISITS_VIDEO_ID), lit("_"),
+        col(Constants.VISITS_DEVICE_ID), lit("_"),
+        col(Constants.VISITS_LOCATION_ID), lit("_"),
+        col(Constants.VISITS_VISIT_TIMESTAMP)
+    ))
+
+
+def normalize_visit(df_visit: DataFrame) -> DataFrame:
+    normalized_visit = add_visit_id(df_visit)
+    return normalized_visit
